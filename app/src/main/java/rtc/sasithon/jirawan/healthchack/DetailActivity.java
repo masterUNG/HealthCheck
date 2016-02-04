da81a7eeabe090d8d2448a1ed5a472e6809b1261
masterUNG <phrombutr@gmail.com>
2016-02-04 12:48:23 +0700

    เพิ่ม ChcekButton

--- app/src/main/java/rtc/sasithon/jirawan/healthchack/DetailActivity.java
+++ app/src/main/java/rtc/sasithon/jirawan/healthchack/DetailActivity.java
@@ -16,6 +16,7 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+
         //Bind Widget
         bindWidget();
 
