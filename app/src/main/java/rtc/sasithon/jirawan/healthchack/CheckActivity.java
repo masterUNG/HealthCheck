fa433a5900a785c3ec91c957a12d54236fb85b85
masterUNG <phrombutr@gmail.com>
2016-02-05 00:13:57 +0700

    Alert Check OK

--- app/src/main/java/rtc/sasithon/jirawan/healthchack/CheckActivity.java
+++ app/src/main/java/rtc/sasithon/jirawan/healthchack/CheckActivity.java
@@ -1,9 +1,15 @@
 package rtc.sasithon.jirawan.healthchack;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -15,6 +21,7 @@ public class CheckActivity extends AppCompatActivity {
     private Context objContext;
     private TextView titleTextView;
     private String titleString;
+    private int scoreAnInt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +36,48 @@ public class CheckActivity extends AppCompatActivity {
         titleString = getIntent().getStringExtra("Title");
         titleTextView.setText(titleString);
 
-        String[] checkStrings = getIntent().getStringArrayExtra("ListCheck");
+        checkStrings = getIntent().getStringArrayExtra("ListCheck");
         ArrayAdapter<String> objArrayAdapter = new ArrayAdapter<String>(this, R.layout.my_check_listview, checkStrings);
         checkListView.setAdapter(objArrayAdapter);
         checkListView.setItemsCanFocus(false);
         checkListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
-        checkListView.setOnItemClickListener(new CheckBoxClick());
+        //checkListView.setOnItemClickListener(new CheckBoxClick());
+        checkListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                CheckedTextView objCheckedTextView = (CheckedTextView) view;
+
+                if (objCheckedTextView.isChecked()) {
+                    scoreAnInt += 1;
+                } else {
+                    scoreAnInt -= 1;
+                }
+
+                Log.d("check", "Score = " + scoreAnInt);
+
+            }   // event
+        });
 
     }   // Main Method
+
+    public void clickHealthCheck(View view) {
+
+        int intFullScore = checkStrings.length;
+
+        AlertDialog.Builder objBuilder = new AlertDialog.Builder(this);
+        objBuilder.setTitle("ความคาดหมาย");
+        objBuilder.setMessage("คะแนนของคุณ = " + Integer.toString(scoreAnInt) + " / " + Integer.toString(intFullScore));
+        objBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        objBuilder.show();
+
+    }   // clickHealthCheck
 
 
 }   // Main Class
